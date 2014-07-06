9919e842ee1df8eb6af739d84fb2ff87cea5bea6
Mateusz Bagiński <cziken58@gmail.com>
2014-07-06 21:24:27 +0200

    distance diffuse

--- shaders/vertex_shader.glsl
+++ shaders/vertex_shader.glsl
@@ -1,16 +1,17 @@
 #version 400
-layout(location = 0) in  vec4 Position;
-layout(location = 1) in  vec3 Normal;
-layout(location = 2) in  vec2 UV;
+layout(location = 0) in  vec4 	Position;
+layout(location = 1) in  vec3 	Normal;
+layout(location = 2) in  vec2 	UV;
 layout(location = 3) in  int	MTL_index;
 
 out 	vec2		frag_uv;
-out	vec3		frag_normal;
+out		vec3		frag_normal;
 out 	vec3		frag_pos;
 out 	float		mtl;
 
 struct Matrix {
 	mat4	 	mvp;
+	mat4		m;
 	mat3		normal;
 };
 uniform Matrix	matrix;
@@ -18,7 +19,7 @@ uniform Matrix	matrix;
 void main(void) {
 	gl_Position 	= 	Position * matrix.mvp;
 	frag_normal		= 	Normal * matrix.normal;
-	frag_pos			=	gl_Position.xyz;
+	frag_pos		=	vec3(Position * matrix.m);
 
 	frag_uv = UV;
 	mtl = MTL_index;
